f44f3de9d43f9786b8678f438c7086e89df9071c
Prime <primevaldigitals@gmail.com>
2026-04-27 21:56:59 -0700

    coding the lodin page

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <LoginPage></LoginPage>
-      <WelcomePage></WelcomePage>
+      {/* <WelcomePage></WelcomePage> */}
 
     </>
   )
--- src/Login-page.jsx
+++ src/Login-page.jsx
@@ -3,9 +3,22 @@ import './Login-page.css'
 function LoginPage() {
   return (
         <>
-            <nav>
-                <img src="/images/logo_nust_png.png" alt="" />
-            </nav>
+            <div id='loginpage'>
+                <nav>
+                    <img src="/logo_nust_png.png" alt="" id='nust-logo' />
+                    <h2>NATIONAL UNIVERSITY OF SCIENCE AND TECHNOLOGY</h2>
+                </nav>
+
+                <div id='main-login-body'>
+                    <div id='login-container'>
+                        <h3>STUDENTS PORTAL</h3>
+                        <input type="text" placeholder='Student Number' />
+                        <input type="text" placeholder='Password' />
+
+                    </div>
+                </div>
+            </div>
+
         </>
   );
 }

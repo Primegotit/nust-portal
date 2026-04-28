c60e2f1e954f3348416f5e945db71e1e0c0cc5e2
Prime <primevaldigitals@gmail.com>
2026-04-27 21:03:19 -0700

    removing all the preexisting css code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import WelcomePage from './Welcome-page'
 
 function App() {
 
   return (
     <>
-      
+      <WelcomePage></WelcomePage>
     </>
   )
 }
---
+++ src/Welcome-page.jsx
@@ -0,0 +1,10 @@
+function WelcomePage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to the about page!</p>
+    </div>
+  );
+}
+
+export default WelcomePage;

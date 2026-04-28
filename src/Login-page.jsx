4ae620b2133afa085df9a7cd8f84741fd59da4df
Prime <primevaldigitals@gmail.com>
2026-04-27 23:33:48 -0700

    fixing the bottom banner

--- src/Login-page.jsx
+++ src/Login-page.jsx
@@ -14,6 +14,7 @@ function LoginPage() {
                     <div>
 
                     </div>
+
                     <div id='login-container'>
 
                         <h3>STUDENTS PORTAL</h3>
@@ -39,6 +40,7 @@ function LoginPage() {
                     </div>
                      
                 </div>
+                
             </div>
 
         </>

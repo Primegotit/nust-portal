865172713b6d627789c9375cbbab993d32874eca
Prime <primevaldigitals@gmail.com>
2026-04-28 21:11:41 -0700

    adding another page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <>
+      <StudentDashboard></StudentDashboard>
       <LoginPage></LoginPage>
       {/* <WelcomePage></WelcomePage> */}
 
---
+++ src/Student-Dashboard.jsx
@@ -0,0 +1,15 @@
+import './Student-Dashboard.css'
+
+
+
+function StudentDashboard(){
+
+    return(
+        <>
+            <h1 style={{color:'red',}}>wassup</h1>
+        </>
+    )
+}
+
+
+export default StudentDashboard;

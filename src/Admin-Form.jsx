a54921581516c5b7b715ff7e460cec04bd36adfc
Prime <primevaldigitals@gmail.com>
2026-05-03 06:42:18 +0200

    making the admin page

---
+++ src/Admin-Form.jsx
@@ -0,0 +1,22 @@
+import './Admin-Form.css';
+
+
+function AdminForm() {
+    return (
+        <div id='Admin-form-page'>
+            <nav>
+
+                <div style={{display:'flex', alignItems:'center', gap:'20PX', padding:'4px 20px'}}>
+                    <h2>Admin Form</h2>
+                    <img src="/logo_nust_png.png" alt="" id='nust-logo'  />
+       
+                </div>
+
+                <div id='bottom-border'></div>
+            </nav>
+        </div>
+    );
+
+}
+
+export default AdminForm;
--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import './App.css'
 import WelcomePage from './Welcome-page'
 import LoginPage from './Login-page'
 import StudentDashboard from './Student-Dashboard'
+import AdminForm from './Admin-Form'
 
 function App() {
 
   return (
     <>
+      <AdminForm></AdminForm>
+
       <LoginPage></LoginPage>
 
       <StudentDashboard></StudentDashboard>
-
       {/* <WelcomePage></WelcomePage> */}
 
     </>
